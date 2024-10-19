c6ab067c32e95eb6507b213ff1a1664e58dfdd8e
nn-287 <100829062+nn-287@users.noreply.github.com>
2024-10-19 15:42:28 +0300

    WiredExpress-nancy-contribution

---
+++ app/Model/Branch.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Branch extends Authenticatable
+{
+    use Notifiable;
+
+    protected $casts = [
+        'coverage' => 'integer',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime'
+    ];
+    
+    public function sub_branches()
+    {
+        return $this->hasMany(SubBranch::class);
+    }
+    
+    public function service_prices()
+    {
+        return $this->hasMany(ServicePrice::class);
+    }
+}
